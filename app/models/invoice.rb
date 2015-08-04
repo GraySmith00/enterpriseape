887c5e73d02a96bab6cbc895a699b3cf4bb5ed1a
Gray Smith <gray.smith00@gmail.com>
2015-08-04 02:39:43 +0000

    define invoice and purchases instance variables in invoices_controller show action and create table for purchases on invoices show page

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ActiveRecord::Base
   
   belongs_to :employee
-  has_many :purchases
+  has_many :purchases, dependent: :destroy
   
 #   validates :date, :company, :tax, :employee_id, :status_type, presence: true
   
